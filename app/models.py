e07627336b58f601cab0afaea5c538e16f71f8eb
Paul Huizinga <huizniga.p@tkpinvestments.com>
2020-06-29 17:26:03 +0200

    added some 'done' items to index.html

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from app import db
 
-class tbl_entity(db.Model):
+class entity(db.Model):
+
+    __tablename__ = 'entities'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), index=True, unique=False)
     lastname = db.Column(db.String(50), index=True, unique=False)
-    address = db.Column(db.String(50), index=True, unique=False)
 
     def __repr__(self):
         return '<entity {}>'.format(self.name)
