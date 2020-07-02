5c747ad2bf4190918dbb6d3565374720511300d7
pmhuizinga <pmhuizinga@hotmail.com>
2020-07-02 22:43:17 +0200

    minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,19 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
-
-# from app import models
 
 def create_app(config_name='default'):
 
     app = Flask(__name__)
-    # app.config.from_object(Config)
+
     app.config.from_object(config[config_name])
-    # db = SQLAlchemy(app)
+
     db.init_app(app)
     migrate = Migrate(app, db)
 
