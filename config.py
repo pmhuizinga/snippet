aead831f1a72f505f4af648a31970cd5fe51f480
Paul Huizinga <huizniga.p@tkpinvestments.com>
2020-07-04 08:44:15 +0200

    added bootstrap links

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #                           'mssql://DESKTOP-DB7OB87\SQLPMH/CRUD?driver=SQL+Server'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -19,17 +23,22 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_ECHO = True
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'mssql://DESKTOP-DB7OB87\SQLPMH/CRUD?driver=SQL+Server'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #                           'mssql://DESKTOP-DB7OB87\SQLPMH/CRUD?driver=SQL+Server'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db?check_same_thread=False')
 
 class ProductionConfig(Config):
     """
     Production configurations
     """
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'mssql://DESKTOP-DB7OB87\SQLPMH/CRUD?driver=SQL+Server'
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #                           'mssql://DESKTOP-DB7OB87\SQLPMH/CRUD?driver=SQL+Server'
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db?check_same_thread=False')
     DEBUG = False
 
 config = {
